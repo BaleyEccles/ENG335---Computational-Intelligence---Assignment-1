5813f4b37357a69356694bbd098118c610a5c183
Baley Eccles <baleycod@gmail.com>
2025-08-18 15:15:55 +1000

    The UI can run the simulink model and display the output.
    Still need to clean it up

--- main.m
+++ main.m
@@ -11,21 +11,10 @@ Marital_Status = 1;
 % targeted_group varibles
 Social = 1;
 
+%% Run Simulink/Fuzzy Model
+open_system('Fuzzy_System');
+
+
 %% UI Stuff
 % Run UI
 app = Fuzzy_UI;
-while (1)
-    Real_Estate_Mortgage = app.RealEstateMortgageEditField.Value;
-    Fixed_Expenses = app.FixedExpensesEditField.Value;
-
-    Age = app.AgeEditField.Value;
-    Num_Children = app.NumberOfChildrenEditField.Value;
-    if (app.CheckIfMarriedCheckBox.Value)
-        Marital_Status = 1;
-    else
-        Marital_Status = 0;
-    end
-    output_data = out.yout.get("Targeted_Group_Output").Values.Data;
-    
-end
-
