eb0ef9980a5eaba8e2c713a5f8ffa33f3ed9a3da
Baley Eccles <baleycod@gmail.com>
2025-08-18 11:27:16 +1000

    Stuff

--- main.m
+++ main.m
@@ -1,10 +1,31 @@
-% Run Ui
+%% Needed Varibles
+% financial_background varibles
 Fixed_Expenses = 1;
 Real_Estate_Mortgage = 1;
 
+% personal_data varibles
 Age = 1;
 Num_Children = 1;
 Marital_Status = 1;
 
+% targeted_group varibles
 Social = 1;
+
+%% UI Stuff
+% Run UI
 app = Fuzzy_UI;
+while (1)
+    Real_Estate_Mortgage = app.RealEstateMortgageEditField.Value;
+    Fixed_Expenses = app.FixedExpensesEditField.Value;
+
+    Age = app.AgeEditField.Value;
+    Num_Children = app.NumberOfChildrenEditField.Value;
+    if (app.CheckIfMarriedCheckBox.Value)
+        Marital_Status = 1;
+    else
+        Marital_Status = 0;
+    end
+    output_data = out.yout.get("Targeted_Group_Output").Values.Data;
+    
+end
+
